30d502e01e881280c29dded0a5d5805bf3537ea5
Oliver Dick <oliver.dick@evilstorm.de>
2015-03-26 22:24:32 +0100

    Integrated the "polls" app / added models "Poll" and "Choice" / added the models to the Django admin interface

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from polls.models import Choice, Poll
 
 # Register your models here.
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ('question', 'pub_date')
+    inlines = [ChoiceInline]
+
+admin.site.register(Poll, PollAdmin)   # make Polls editable in admin site
