c60ef89b3e2d4af305525af43ecbef9f2c7675ae
Oliver Dick <oliver.dick@evilstorm.de>
2015-03-26 23:15:07 +0100

    Added some views for the "polls" app

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello World")
+
+def detail(request, poll_id):
+    return HttpResponse("You're looking at poll %s." % poll_id)
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse("You're voting on poll %s." % poll_id)
