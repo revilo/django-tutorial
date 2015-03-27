ca15c4caf42a718be4eed2ce271ec5081fc7b4ad
Oliver Dick <oliver.dick@evilstorm.de>
2015-03-27 18:24:11 +0100

    [polls] Use templates for views "index" and "details"

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http.response import HttpResponse
+from models import Poll
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    context = { 'latest_poll_list': latest_poll_list }
+    return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    poll = get_object_or_404(Poll, pk=poll_id)
+    context = { 'poll': poll }
+    return render(request, 'polls/detail.html', context)
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
